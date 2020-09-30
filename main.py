e557be3f2033f0d9d70f3ac1c0f5f36f4c4f94cf
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2020-09-30 18:10:12 -0400

    Added some variables, print, and concatenate

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 # Printing stuff
 print("Test")
+
 # A comment shortcut in Pycharm is control + /
+
+# Variables
+# Use underscores instead of Camelcase
+my_cats_name = "Orby"
+my_cats_age = 4
+orby_likes_water = False
+orby_like_cheese = True
+orby_can_run = 10.5
+
+print("My cats name is " + my_cats_name)
+# String concatenate using f-strings
+print(f'My cats name is {my_cats_name} and she is {my_cats_age} years old. She can run {orby_can_run} miles per hour')
+

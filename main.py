77e9dc9621871d2087056707d8d29ab3e7159ef4
Mike Hotchkiss <michael.hotchkiss@protonmail.com>
2020-09-30 18:15:43 -0400

    working with more string stuff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# Printing stuff
-print("Test")
+# This is code that I wrote / played with when taking a freecodecamp Python course to refresh my memory
 
 # A comment shortcut in Pycharm is control + /
 
@@ -11,7 +10,15 @@ orby_likes_water = False
 orby_like_cheese = True
 orby_can_run = 10.5
 
+# Strings
 print("My cats name is " + my_cats_name)
 # String concatenate using f-strings
 print(f'My cats name is {my_cats_name} and she is {my_cats_age} years old. She can run {orby_can_run} miles per hour')
+# \n = new line
+# \ is an escape character
+# Printing a variable
+print(my_cats_age)
+# Turning Orby's name into all uppercase
+print(my_cats_name.upper())
 
+# Functions
